67561aaf65b87227a09eb5b3294e503a01f7e76e
Ruben Korse <121227805+RubenKorse@users.noreply.github.com>
2025-03-14 14:50:31 +0100

    admin nav fix

--- resources/views/layouts/adminnav.blade.php
+++ resources/views/layouts/adminnav.blade.php
@@ -13,6 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('User Dashboard') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Admin Dashboard') }}
                     </x-nav-link>
                 </div>

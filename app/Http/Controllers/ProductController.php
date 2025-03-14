f816c5b1bcab74e8721a59583bbc3c93cf6fe91c
Maikel Mureau <maikelmureau@gmail.com>
2025-03-14 13:57:21 +0100

    model fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('products.index', [
@@ -17,17 +14,11 @@ class ProductController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('products.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -63,9 +54,6 @@ class ProductController extends Controller
         return redirect()->route('products.index')->with('success', 'Product succesvol aangemaakt!');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show($id)
     {
         $product = Product::findOrFail($id);
@@ -73,19 +61,12 @@ class ProductController extends Controller
         return view('products.show', compact('product'));
     }
 
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
     {
         $product = Product::findOrFail($id);
         return view('products.edit', compact('product'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, $id)
     {
         $product = Product::findOrFail($id);
@@ -123,8 +104,11 @@ class ProductController extends Controller
         return redirect()->route('products.index')->with('success', 'Product succesvol bijgewerkt!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Product $product) {}
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return redirect()->route('products.index')->with('success', 'Product succesvol verwijderd!');
+    }
 }
--- app/Models/Order_Status.php
+++ app/Models/Order_Status.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order_Status extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }

cc41fc3244ad716f6c6e837fd93fae5d2d5c5c1a
Vicky <116161445+aryasgrave@users.noreply.github.com>
2025-03-14 14:55:42 +0100

    Login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,14 @@
 <x-guest-layout>
     <div class="flex h-screen">
         <!-- Left Side - Orange Background -->
-        <div class="w-1/2 bg-orange-500 flex items-center justify-center">
-            <h1 class="text-5xl font-bold text-white">Welcome Back!</h1>
+        <div class="w-1/2 bg-white flex items-center justify-center">
+            <h1 class="text-5xl font-bold text-black">Welcome Back!</h1>
         </div>
 
         <!-- Right Side - Form -->
-        <div class="w-1/2 flex items-center justify-center bg-gray-100 dark:bg-gray-900">
-            <form method="POST" action="{{ route('login') }}" class="w-3/4 max-w-md bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
+        <div class="w-1/2 flex items-center justify-center bg-black">
+            <form method="POST" action="{{ route('login') }}"
+                class="w-3/4 max-w-md bg-black border-[#3E3E3A] p-12 text-white border rounded-lg shadow-md">
                 @csrf
 
                 <!-- Session Status -->
@@ -16,21 +17,23 @@
                 <!-- Email Address -->
                 <div>
                     <x-input-label for="Name" :value="__('Name')" />
-                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="username" />
+                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                        :value="old('name')" required autofocus />
                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
                 </div>
 
                 <!-- Password -->
                 <div class="mt-4">
                     <x-input-label for="password" :value="__('Password')" />
-                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
                 <!-- Remember Me -->
                 <div class="block mt-4">
                     <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-orange-600 shadow-sm focus:ring-orange-500" name="remember">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-black shadow-sm"
+                            name="remember">
                         <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
                     </label>
                 </div>
@@ -38,12 +41,13 @@
                 <!-- Forgot Password and Submit -->
                 <div class="flex items-center justify-between mt-4">
                     @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" href="{{ route('password.request') }}">
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+                            href="{{ route('password.request') }}">
                             {{ __('Forgot your password?') }}
                         </a>
                     @endif
 
-                    <x-primary-button class="ml-3 bg-orange-500 hover:bg-orange-600">
+                    <x-primary-button class="ml-3 bg-white text-black">
                         {{ __('Log in') }}
                     </x-primary-button>
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,125 @@
 <x-guest-layout>
-    <section class="w-full grid grid-cols-12 gap-6">
-        <form method="GET" action="{{ route('welcome') }}" class="col-span-12 grid grid-cols-12 gap-6 w-full mb-6"
-            id="filterForm">
-            <!-- Search Bar -->
-            <div class="mb-4 col-span-12 flex flex-row gap-2">
-                <input type="text" name="search" placeholder="Zoek producten" value="{{ request('search') }}"
-                    class="w-full  bg-transparent inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
-                <button type="submit"
-                    class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300 ">Filter</button>
-            </div>
-
-            <!-- Filter by Product Type -->
-            <div class="mb-4 col-span-3">
-                <label for="product_type" class="block text-white mb-2">Types</label>
-                <select name="product_type"
-                    class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
-                    <option value="">All types</option>
-                    @foreach ($productTypes as $type)
-                        <option value="{{ $type->id }}"
-                            {{ request('product_type') == $type->id ? 'selected' : '' }}>
-                            {{ $type->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-
-            <!-- Filter by Complexity -->
-            <div class="mb-4 col-span-3">
-                <label for="complexity" class="block text-white mb-2">complexities</label>
-                <select name="complexity"
-                    class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
-                    <option value="">All complexities</option>
-                    @foreach ($complexityOptions as $complexity)
-                        <option value="{{ $complexity }}"
-                            {{ request('complexity') == $complexity ? 'selected' : '' }}>
-                            {{ $complexity }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-
-            <!-- Filter by Sustainability -->
-            <div class="mb-4 col-span-3">
-                <label for="sustainability" class="block text-white mb-2">Sustainability</label>
-                <select name="sustainability"
-                    class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
-                    <option value="">All sustainability Options</option>
-                    @foreach ($sustainabilityOptions as $sustainability)
-                        <option value="{{ $sustainability }}"
-                            {{ request('sustainability') == $sustainability ? 'selected' : '' }}>
-                            {{ $sustainability }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-
-            <!-- Order by Price -->
-            <div class="mb-4 col-span-3">
-                <label for="price" class="block text-white mb-2">Price</label>
-                <select name="price"
-                    class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
-                    <option value="">All prices</option>
-                    <option value="high" {{ request('price') == 'high' ? 'selected' : '' }}>High to low</option>
-                    <option value="low" {{ request('price') == 'low' ? 'selected' : '' }}>Low to high</option>
-                </select>
-            </div>
-        </form>
-
-        <!-- Display filtered products -->
-        @foreach ($products as $product)
-            <div
-                class="col-span-4 inline-block px-5 py-1.5 border-[#3E3E3A] text-white border rounded-sm text-sm leading-normal transition-all duration-300">
-                <div>
-                    @if ($product->image)
-                        <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"
-                            class="w-full h-auto rounded-lg shadow">
+    <section class="flex flex-col items-center justify-center container lg:px-6 sm:px-4">
+        <header class="w-full text-sm my-6">
+            @if (Route::has('login'))
+                <nav class="flex items-center justify-end gap-4">
+                    @auth
+                        <a href="{{ url('/dashboard') }}"
+                            class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300 ">
+                            Dashboard
+                        </a>
                     @else
-                        <p class="text-gray-500">No images available</p>
-                    @endif
+                        <a href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300">
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                                class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300">
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </nav>
+            @endif
+        </header>
+
+        <section class="w-full grid grid-cols-12 gap-6  mb-20">
+            <form method="GET" action="{{ route('welcome') }}" class="col-span-12 grid grid-cols-12 gap-6 w-full mb-6"
+                id="filterForm">
+                <!-- Search Bar -->
+                <div class="mb-4 col-span-12 flex flex-row gap-2">
+                    <input type="text" name="search" placeholder="Zoek producten" value="{{ request('search') }}"
+                        class="w-full  bg-transparent inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
+                    <button type="submit"
+                        class="flex flex-row gap-3 items-center px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300 "><i
+                            class="fa-solid fa-magnifying-glass"></i>Search</button>
                 </div>
 
-                <h1 class="text-2xl font-bold mb-4">{{ $product->name }}</h1>
-                <p><strong>Description:</strong> {{ $product->description }}</p>
-                <p><strong>Type:</strong> {{ $product->type_name }}</p>
-                <p><strong>Material:</strong> {{ $product->material }}</p>
-                <p><strong>Production time:</strong> {{ $product->production_time }}</p>
-                <p><strong>Complexity:</strong> {{ $product->complexity }}</p>
-                <p><strong>Sustainability:</strong> {{ $product->sustainability }}</p>
-                <p><strong>Unique properties:</strong> {{ $product->unique_properties }}</p>
-                <p class="text-lg font-semibold"><strong>Price:</strong> €{{ number_format($product->price, 2) }}</p>
-            </div>
-        @endforeach
+                <!-- Filter by Product Type -->
+                <div class="mb-4 col-span-3">
+                    <label for="product_type" class="block text-white mb-2">Types</label>
+                    <select name="product_type"
+                        class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
+                        <option value="">All types</option>
+                        @foreach ($productTypes as $type)
+                            <option value="{{ $type->id }}"
+                                {{ request('product_type') == $type->id ? 'selected' : '' }}>
+                                {{ $type->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <!-- Filter by Complexity -->
+                <div class="mb-4 col-span-3">
+                    <label for="complexity" class="block text-white mb-2">complexities</label>
+                    <select name="complexity"
+                        class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
+                        <option value="">All complexities</option>
+                        @foreach ($complexityOptions as $complexity)
+                            <option value="{{ $complexity }}"
+                                {{ request('complexity') == $complexity ? 'selected' : '' }}>
+                                {{ $complexity }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <!-- Filter by Sustainability -->
+                <div class="mb-4 col-span-3">
+                    <label for="sustainability" class="block text-white mb-2">Sustainability</label>
+                    <select name="sustainability"
+                        class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
+                        <option value="">All sustainability Options</option>
+                        @foreach ($sustainabilityOptions as $sustainability)
+                            <option value="{{ $sustainability }}"
+                                {{ request('sustainability') == $sustainability ? 'selected' : '' }}>
+                                {{ $sustainability }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <!-- Order by Price -->
+                <div class="mb-4 col-span-3">
+                    <label for="price" class="block text-white mb-2">Price</label>
+                    <select name="price"
+                        class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
+                        <option value="">All prices</option>
+                        <option value="high" {{ request('price') == 'high' ? 'selected' : '' }}>High to low</option>
+                        <option value="low" {{ request('price') == 'low' ? 'selected' : '' }}>Low to high</option>
+                    </select>
+                </div>
+            </form>
+
+            <!-- Display filtered products -->
+            @foreach ($products as $product)
+                <div
+                    class="col-span-4 inline-block px-5 py-1.5 border-[#3E3E3A] text-white border rounded-sm text-sm leading-normal transition-all duration-300">
+                    <div>
+                        @if ($product->image)
+                            <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"
+                                class="w-full h-auto rounded-lg shadow">
+                        @else
+                            <p class="text-gray-500">No images available</p>
+                        @endif
+                    </div>
+
+                    <h1 class="text-2xl font-bold mb-4">{{ $product->name }}</h1>
+                    <p><strong>Description:</strong> {{ $product->description }}</p>
+                    <p><strong>Type:</strong> {{ $product->type_name }}</p>
+                    <p><strong>Material:</strong> {{ $product->material }}</p>
+                    <p><strong>Production time:</strong> {{ $product->production_time }}</p>
+                    <p><strong>Complexity:</strong> {{ $product->complexity }}</p>
+                    <p><strong>Sustainability:</strong> {{ $product->sustainability }}</p>
+                    <p><strong>Unique properties:</strong> {{ $product->unique_properties }}</p>
+                    <p class="text-lg font-semibold"><strong>Price:</strong> €{{ number_format($product->price, 2) }}
+                    </p>
+                </div>
+            @endforeach
+        </section>
     </section>
 
 

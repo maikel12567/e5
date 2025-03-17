ea34cb923b0e3508a6c0191294829788d13aa281
Maikel Mureau <maikelmureau@gmail.com>
2025-03-17 09:01:50 +0100

    fix: header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,33 @@
 <x-guest-layout>
     <section class="flex flex-col items-center justify-center container lg:px-6 sm:px-4">
-        <header class="w-full text-sm my-6">
-            @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
-                    @auth
-                        <a href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300 ">
-                            Dashboard
-                        </a>
-                    @else
-                        <a href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300">
-                            Log in
-                        </a>
+    <header class="w-full text-sm my-6 flex justify-between items-center">
+    <h2 class="text-4xl font-bold text-white flex-1">Makersmarkt</h2>
+    
+    @if (Route::has('login'))
+    <nav class="flex items-center gap-4">
+        @auth
+        <a href="{{ url('/dashboard') }}"
+            class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300">
+            Dashboard
+        </a>
+        @else
+        <a href="{{ route('login') }}"
+            class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300">
+            Log in
+        </a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300">
-                                Register
-                            </a>
-                        @endif
-                    @endauth
-                </nav>
-            @endif
-        </header>
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}"
+            class="inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm text-sm leading-normal transition-all duration-300">
+            Register
+        </a>
+        @endif
+        @endauth
+    </nav>
+    @endif
+</header>
+
+
 
         <section class="w-full grid grid-cols-12 gap-6  mb-20">
             <form method="GET" action="{{ route('welcome') }}" class="col-span-12 grid grid-cols-12 gap-6 w-full mb-6"
@@ -44,10 +48,10 @@
                         class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
                         <option value="">All types</option>
                         @foreach ($productTypes as $type)
-                            <option value="{{ $type->id }}"
-                                {{ request('product_type') == $type->id ? 'selected' : '' }}>
-                                {{ $type->name }}
-                            </option>
+                        <option value="{{ $type->id }}"
+                            {{ request('product_type') == $type->id ? 'selected' : '' }}>
+                            {{ $type->name }}
+                        </option>
                         @endforeach
                     </select>
                 </div>
@@ -59,10 +63,10 @@
                         class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
                         <option value="">All complexities</option>
                         @foreach ($complexityOptions as $complexity)
-                            <option value="{{ $complexity }}"
-                                {{ request('complexity') == $complexity ? 'selected' : '' }}>
-                                {{ $complexity }}
-                            </option>
+                        <option value="{{ $complexity }}"
+                            {{ request('complexity') == $complexity ? 'selected' : '' }}>
+                            {{ $complexity }}
+                        </option>
                         @endforeach
                     </select>
                 </div>
@@ -74,10 +78,10 @@
                         class="w-full  bg-black inline-block px-5 py-1.5 border-[#3E3E3A] hover:border-gray-300 text-white border rounded-sm transition-all duration-300">
                         <option value="">All sustainability Options</option>
                         @foreach ($sustainabilityOptions as $sustainability)
-                            <option value="{{ $sustainability }}"
-                                {{ request('sustainability') == $sustainability ? 'selected' : '' }}>
-                                {{ $sustainability }}
-                            </option>
+                        <option value="{{ $sustainability }}"
+                            {{ request('sustainability') == $sustainability ? 'selected' : '' }}>
+                            {{ $sustainability }}
+                        </option>
                         @endforeach
                     </select>
                 </div>
@@ -96,33 +100,33 @@
 
             <!-- Display filtered products -->
             @foreach ($products as $product)
-                <div
-                    class="col-span-4 inline-block px-5 py-1.5 border-[#3E3E3A] text-white border rounded-sm text-sm leading-normal transition-all duration-300">
-                    <div>
-                        @if ($product->image)
-                            <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"
-                                class="w-full h-auto rounded-lg shadow">
-                        @else
-                            <p class="text-gray-500">No images available</p>
-                        @endif
-                    </div>
+            <div
+                class="col-span-4 inline-block px-5 py-1.5 border-[#3E3E3A] text-white border rounded-sm text-sm leading-normal transition-all duration-300">
+                <img src="https://picsum.photos/seed/{{ $product->id }}/1300/600" alt="Product afbeelding" class="rounded">
 
-                    <h1 class="text-2xl font-bold mb-4">{{ $product->name }}</h1>
-                    <p><strong>Description:</strong> {{ $product->description }}</p>
-                    <p><strong>Sustainability:</strong> {{ $product->sustainability }}</p>
-                    <div class="flex flex-row gap-3 my-4">
-                        <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-puzzle-piece"></i></strong>
-                            {{ $product->complexity }}</p>
-                        <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-file"></i></strong>
-                            {{ $product->type_name }}</p>
-                        <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-gem"></i></strong>
-                            {{ $product->material }}</p>
-                        <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-clock"></i></strong>
-                            {{ $product->production_time }}</p>
-                    </div>
-                    <p class="text-lg font-semibold"><strong>Price:</strong> €{{ number_format($product->price, 2) }}
+                <h1 class="text-2xl font-bold mb-4">{{ $product->name }}</h1>
+                <p><strong>Description:</strong> {{ $product->description }}</p>
+                <p><strong>Sustainability:</strong> {{ $product->sustainability }}</p>
+                <div class="flex flex-row gap-3 my-4">
+                    <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-puzzle-piece"></i></strong>
+                        {{ $product->complexity }}
+                    </p>
+                    <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-file"></i></strong>
+                        {{ $product->type_name }}
+                    </p>
+                    <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-gem"></i></strong>
+                        {{ $product->material }}
+                    </p>
+                    <p class="flex flex-row gap-2"><strong><i class="fa-solid fa-clock"></i></strong>
+                        {{ $product->production_time }}
                     </p>
                 </div>
+                <div class="flex flex-row justify-between items-center">
+                    <p class="text-lg font-semibold"><strong>Price:</strong> €{{ number_format($product->price, 2) }}
+                    </p>
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded">show more</button>
+                </div>
+            </div>
             @endforeach
         </section>
     </section>
